537db2559c4c0686a2f20ee34b97fde0ed0ea7aa
dennis.obel <dennis.obel@gmail.com>
2022-03-25 08:23:11 +0300

    header footer changes

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,6 @@
 const apiConfig = {
     baseUrl: 'https://api.themoviedb.org/3/',
-    apiKey: 'get from themoviedb.org',
+    apiKey: '4536f108a28f0c0dfa9d4508d4b149ba',
     originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
     w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
 }
