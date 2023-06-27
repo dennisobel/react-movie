09c8ad871e9cc723e605d96610f1efad5a33e294
Dennis Obel <ogembodenis2016@gmail.com>
2023-06-27 12:07:37 +0300

    Update apiConfig.js

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -2,7 +2,7 @@ const apiConfig = {
     baseUrl: 'https://api.themoviedb.org/3/',
     apiKey: '4536f108a28f0c0dfa9d4508d4b149ba',
     originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
-    w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
+    w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`,
 }
 
 export default apiConfig;
